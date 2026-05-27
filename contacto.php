6632fb4888997c90afea48ad5b055d445c969394
Rosabel <rcaroc@ucvvirtual.edu.pe>
2026-05-27 16:02:26 -0500

    se agrego contacto

---
+++ contacto.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Contacto</title>
+</head>
+<body>
+    <h1>Formulario de Contacto</h1>
+    <form action="#" method="POST">
+        <input type="text" placeholder="Tu nombre" required><br><br>
+        <textarea placeholder="Tu mensaje"></textarea><br><br>
+        <button type="submit">Enviar</button>
+    </form>
+    <br>
+    <a href="index.php">Volver al inicio</a>
+</body>
+</html>
